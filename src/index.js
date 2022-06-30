41bf282cff6ff4b186348ebc3cb8430f12e331c2
Lupanova Anna <hanna.lupanava@newland.by>
2022-06-30 14:59:33 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+const dict = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+    '|': '|'
 }
+module.exports = function check(str, bracketsConfig) {
+    let dict = {}
+    let stack = []
+    bracketsConfig.forEach((item,index)=> {
+
+        dict[item[0].toString()] = item[1]
+    })
+
+    let open = bracketsConfig.reduce((acc,cur)=> acc.concat(cur[0]),[])
+    let close = bracketsConfig.reduce((acc,cur)=> acc.concat(cur[1]),[])
+    for (let bracket of str){
+        if (open.includes(bracket) && close.includes(bracket) ){
+            if (!stack.includes(bracket)){
+                stack.push(bracket)
+            }
+            else{
+                stack.pop()
+            }
+        }
+        else{
+
+
+            if (open.includes(bracket)){
+
+                stack.push(bracket)
+            }
+            else {
+                pop_el = stack.pop()
+                if (dict[pop_el] === bracket){
+                    continue
+                }
+                else{
+                    return false
+                }
+            }
+        }
+
+
+    }
+    return stack.length === 0
+}
+
+
+
